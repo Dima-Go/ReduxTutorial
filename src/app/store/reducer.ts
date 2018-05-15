f5f7c45b28d6402faff7c5870fdc6974e5fbc962
Dima Gonikman <dima.gonikman@gmail.com>
2018-05-15 17:33:26 +0300

    Added store, reducer and store subscription

---
+++ src/app/store/reducer.ts
@@ -0,0 +1,29 @@
+import { Course } from '../courses/course';
+import { IAppState } from './IAppState';
+
+const initialState: IAppState =
+{
+    courses:
+    [
+        {
+          "id": 1,
+          "name": "Building Apps with React (local)",
+          "topic": "ReactJS"
+        },
+        {
+          "id": 2,
+          "name": "Building Apps with Angulart (local)",
+          "topic": "AngularJS"
+        },
+        {
+          "id":3,
+          "name": "Building Apps with Angular and Reduxt (local)",
+          "topic": "Angular and Redux"
+        }
+      ]
+};
+
+export function reducer(state = initialState, action)
+{
+    return state;
+}
